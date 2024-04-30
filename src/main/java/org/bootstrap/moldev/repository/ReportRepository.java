37d360188b3c2bd1d45ce396710ba3ff232553f1
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-04-30 11:00:58 +0900

    KBB-42 [Feat] Report repository 추가

---
+++ src/main/java/org/bootstrap/moldev/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package org.bootstrap.moldev.repository;
+
+import org.bootstrap.moldev.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
