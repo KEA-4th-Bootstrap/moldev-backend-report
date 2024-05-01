0ed5405cf74228f23bad319ba58ddfc2842c1a69
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-02 02:22:20 +0900

    KBB-55 [Feat] submitReportPost 추가

--- src/main/java/org/bootstrap/moldev/controller/ReportController.java
+++ src/main/java/org/bootstrap/moldev/controller/ReportController.java
@@ -1,6 +1,13 @@
 package org.bootstrap.moldev.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.moldev.common.SuccessResponse;
+import org.bootstrap.moldev.dto.request.SubmitReportPostRequestDto;
+import org.bootstrap.moldev.dto.response.SubmitReportPostResponseDto;
+import org.bootstrap.moldev.service.ReportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/report")
 public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/report-post")
+    public ResponseEntity<SuccessResponse<?>> submitReportPost(@RequestBody SubmitReportPostRequestDto submitReportPostRequestDto) {
+        SubmitReportPostResponseDto submitReportPostResponseDto = reportService.submitReportPost(submitReportPostRequestDto);
+        return SuccessResponse.created(submitReportPostResponseDto);
+    }
 }
