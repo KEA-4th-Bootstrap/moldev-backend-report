c68917d80e5c7266ffcb2f87456ecb2a02f77356
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-02 14:10:49 +0900

    KBB-57 [Feat] 댓글 신고 기능 구현

--- src/main/java/org/bootstrap/moldev/controller/ReportController.java
+++ src/main/java/org/bootstrap/moldev/controller/ReportController.java
@@ -3,7 +3,9 @@ package org.bootstrap.moldev.controller;
 import lombok.RequiredArgsConstructor;
 import org.bootstrap.moldev.common.SuccessResponse;
 import org.bootstrap.moldev.dto.request.SubmitReportPostRequestDto;
+import org.bootstrap.moldev.dto.request.SubmitReportReplyRequestDto;
 import org.bootstrap.moldev.dto.response.SubmitReportPostResponseDto;
+import org.bootstrap.moldev.dto.response.SubmitReportReplyResponseDto;
 import org.bootstrap.moldev.service.ReportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,5 +24,11 @@ public class ReportController {
     public ResponseEntity<SuccessResponse<?>> submitReportPost(@RequestBody SubmitReportPostRequestDto submitReportPostRequestDto) {
         SubmitReportPostResponseDto submitReportPostResponseDto = reportService.submitReportPost(submitReportPostRequestDto);
         return SuccessResponse.created(submitReportPostResponseDto);
+    }
+
+    @PostMapping("/reply")
+    public ResponseEntity<SuccessResponse<?>> submitReportReply(@RequestBody SubmitReportReplyRequestDto submitReportReplyRequestDto) {
+        SubmitReportReplyResponseDto submitReportReplyResponseDto = reportService.submitReportReply(submitReportReplyRequestDto);
+        return SuccessResponse.created(submitReportReplyResponseDto);
     }
 }
