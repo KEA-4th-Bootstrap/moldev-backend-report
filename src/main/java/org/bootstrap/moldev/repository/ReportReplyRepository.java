23d7d4580960f82d7f3c5ea604b39e31fcf84b7f
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-06 16:03:40 +0900

    KBB-63 [Refactor] Report 테이블 로직 변경으로 인한 통합

--- src/main/java/org/bootstrap/moldev/repository/ReportReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package org.bootstrap.moldev.repository;
-
-import org.bootstrap.moldev.entity.ReportReply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportReplyRepository extends JpaRepository<ReportReply, Long> {
-}
